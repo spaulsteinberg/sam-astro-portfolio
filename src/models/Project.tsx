a78a3eccb0e3beb077c9e3f40c023da5a584d2d2
spaulsteinberg <sampaulsteinberg@gmail.com>
2022-08-31 12:48:43 -0500

    tile updates

--- src/models/Project.tsx
+++ src/models/Project.tsx
@@ -1,8 +1,11 @@
+import type ProjectImage from "./ProjectImage";
+
 interface Project {
     route:string;
     type:string;
     title:string;
     colorClass:string;
+    image:ProjectImage;
 }
 
 export default Project
