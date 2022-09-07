32d6985cd878977287fca4ae3b201eea6f267490
spaulsteinberg <sampaulsteinberg@gmail.com>
2022-09-06 21:02:13 -0500

    close #1

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,6 +1,35 @@
 import { h } from "preact";
 import "./Nav.scss"
 
+const NAV_LINKS = [
+    {
+        route: "/",
+        title: "Work"
+    },
+    {
+        route: "/about",
+        title: "About"
+    },
+    {
+        route: "/experience",
+        title: "Experience"
+    },
+    {
+        route: "/contact",
+        title: "Contact"
+    }
+]
+
+const NavLinkItem = ({ route, title }) => {
+    return (
+        <li class="nav-item">
+            <a class="nav-link text-white px-4" href={route}>
+                {title}
+            </a>
+        </li>
+    )
+}
+
 const Nav = () => {
 
     return (
@@ -13,28 +42,16 @@ const Nav = () => {
                 aria-controls="navbarSupportedContent"
                 aria-expanded="false"
                 aria-label="Toggle navigation"
-                style={{marginLeft: '1rem'}}
+                style={{ marginLeft: '1rem' }}
             >
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div id="navbarSupportedContent" className="collapse navbar-collapse">
                 <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link text-white px-4" href="/">
-                            Work
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white px-4" href="/about">
-                            About
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-white px-4" href="/experience">
-                            Experience
-                        </a>
-                    </li>
+                    {
+                        NAV_LINKS.map(link => <NavLinkItem key={link.title} route={link.route} title={link.title} />)
+                    }
                 </ul>
             </div>
         </nav>
